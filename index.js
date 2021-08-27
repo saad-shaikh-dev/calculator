3ea9bf5351aa8d2e3dbb3edd602a90d9dcb9977f
Saad <58905892+saadshaikh1992@users.noreply.github.com>
2021-08-27 16:50:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
-const one = "1";
-const two = "2";
-let calculation = one + two;
+document.getElementById("one").addEventListener("click", () => {
+  document.getElementById("calculation").value += "1";
+});
+document.getElementById("two").addEventListener("click", () => {
+  document.getElementById("calculation").value += "2";
+});
+document.getElementById("three").addEventListener("click", () => {
+  document.getElementById("calculation").value += "3";
+});
+document.getElementById("four").addEventListener("click", () => {
+  document.getElementById("calculation").value += "4";
+});
+document.getElementById("five").addEventListener("click", () => {
+  document.getElementById("calculation").value += "5";
+});
+document.getElementById("six").addEventListener("click", () => {
+  document.getElementById("calculation").value += "6";
+});
+document.getElementById("seven").addEventListener("click", () => {
+  document.getElementById("calculation").value += "7";
+});
+document.getElementById("eight").addEventListener("click", () => {
+  document.getElementById("calculation").value += "8";
+});
+document.getElementById("nine").addEventListener("click", () => {
+  document.getElementById("calculation").value += "9";
+});
+document.getElementById("period").addEventListener("click", () => {
+  document.getElementById("calculation").value += ".";
+});
+document.getElementById("add").addEventListener("click", () => {
+  document.getElementById("calculation").value += "+";
+});
+document.getElementById("subtract").addEventListener("click", () => {
+  document.getElementById("calculation").value += "-";
+});
+document.getElementById("multiply").addEventListener("click", () => {
+  document.getElementById("calculation").value += "*";
+});
+document.getElementById("divide").addEventListener("click", () => {
+  document.getElementById("calculation").value += "/";
+});
 
-console.log(calculation);
+//clear calculation
+document.getElementById("clear").addEventListener("click", () => {
+  document.getElementById("calculation").value = "0";
+});
 
-const result = parseInt(calculation);
-console.log(result);
+// get result
+document.getElementById("getResult").addEventListener("click", () => {
+  document.getElementById("result").value = eval(document.getElementById("calculation").value);
+});
