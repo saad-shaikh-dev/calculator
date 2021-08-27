af277654d2b6d0118ed05a24f54d17c9382bc76b
Saad <58905892+saadshaikh1992@users.noreply.github.com>
2021-08-27 18:36:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
+// get result
+function getResult() {
+  document.getElementById("result").value = eval(document.getElementById("calculation").value);
+}
+// input function
+document.getElementById("zero").addEventListener("click", () => {
+  document.getElementById("calculation").value += "0";
+  getResult();
+});
 document.getElementById("one").addEventListener("click", () => {
   document.getElementById("calculation").value += "1";
+  getResult();
 });
 document.getElementById("two").addEventListener("click", () => {
   document.getElementById("calculation").value += "2";
+  getResult();
 });
 document.getElementById("three").addEventListener("click", () => {
   document.getElementById("calculation").value += "3";
+  getResult();
 });
 document.getElementById("four").addEventListener("click", () => {
   document.getElementById("calculation").value += "4";
+  getResult();
 });
 document.getElementById("five").addEventListener("click", () => {
   document.getElementById("calculation").value += "5";
+  getResult();
 });
 document.getElementById("six").addEventListener("click", () => {
   document.getElementById("calculation").value += "6";
+  getResult();
 });
 document.getElementById("seven").addEventListener("click", () => {
   document.getElementById("calculation").value += "7";
+  getResult();
 });
 document.getElementById("eight").addEventListener("click", () => {
   document.getElementById("calculation").value += "8";
+  getResult();
 });
 document.getElementById("nine").addEventListener("click", () => {
   document.getElementById("calculation").value += "9";
+  getResult();
 });
 document.getElementById("period").addEventListener("click", () => {
   document.getElementById("calculation").value += ".";
@@ -42,14 +60,11 @@ document.getElementById("divide").addEventListener("click", () => {
 });
 document.getElementById("percentage").addEventListener("click", () => {
   document.getElementById("calculation").value += "*(1/100)";
+  getResult();
 });
 
 //clear calculation
 document.getElementById("clear").addEventListener("click", () => {
   document.getElementById("calculation").value = "0";
-});
-
-// get result
-document.getElementById("getResult").addEventListener("click", () => {
-  document.getElementById("result").value = eval(document.getElementById("calculation").value);
+  getResult();
 });
