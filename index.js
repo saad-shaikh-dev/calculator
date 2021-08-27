222e51cc1bb82f04f42ee5cbcd9563e8b0671723
Saad <58905892+saadshaikh1992@users.noreply.github.com>
2021-08-27 19:14:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+// get result
+function getResult() {
+  document.getElementById("result").value = eval(
+    document.getElementById("calculation").value
+  );
+}
+// input function
+document.getElementById("zero").addEventListener("click", () => {
+  document.getElementById("calculation").value += "0";
+  getResult();
+});
+document.getElementById("one").addEventListener("click", () => {
+  document.getElementById("calculation").value += "1";
+  getResult();
+});
+document.getElementById("two").addEventListener("click", () => {
+  document.getElementById("calculation").value += "2";
+  getResult();
+});
+document.getElementById("three").addEventListener("click", () => {
+  document.getElementById("calculation").value += "3";
+  getResult();
+});
+document.getElementById("four").addEventListener("click", () => {
+  document.getElementById("calculation").value += "4";
+  getResult();
+});
+document.getElementById("five").addEventListener("click", () => {
+  document.getElementById("calculation").value += "5";
+  getResult();
+});
+document.getElementById("six").addEventListener("click", () => {
+  document.getElementById("calculation").value += "6";
+  getResult();
+});
+document.getElementById("seven").addEventListener("click", () => {
+  document.getElementById("calculation").value += "7";
+  getResult();
+});
+document.getElementById("eight").addEventListener("click", () => {
+  document.getElementById("calculation").value += "8";
+  getResult();
+});
+document.getElementById("nine").addEventListener("click", () => {
+  document.getElementById("calculation").value += "9";
+  getResult();
+});
+document.getElementById("period").addEventListener("click", () => {
+  document.getElementById("calculation").value += ".";
+});
+document.getElementById("add").addEventListener("click", () => {
+  document.getElementById("calculation").value += "+";
+});
+document.getElementById("subtract").addEventListener("click", () => {
+  document.getElementById("calculation").value += "-";
+});
+document.getElementById("multiply").addEventListener("click", () => {
+  document.getElementById("calculation").value += "*";
+});
+document.getElementById("divide").addEventListener("click", () => {
+  document.getElementById("calculation").value += "/";
+});
+document.getElementById("percentage").addEventListener("click", () => {
+  document.getElementById("calculation").value += "*(1/100)";
+  getResult();
+});
+document.getElementById("openBracket").addEventListener("click", () => {
+  document.getElementById("calculation").value += "(";
+});
+document.getElementById("closeBracket").addEventListener("click", () => {
+  document.getElementById("calculation").value += ")";
+  getResult();
+});
+document.getElementById("changeSign").addEventListener("click", () => {
+  document.getElementById("calculation").value += "*(-1)";
+  getResult();
+});
+
+//clear calculation
+document.getElementById("clear").addEventListener("click", () => {
+  document.getElementById("calculation").value = "";
+  document.getElementById("result").value = 0;
+});
